3b39e859c3e3266d7875324ad23071eaa7dd0549
James Coliz <jcoliz@outlook.com>
2025-06-20 15:05:03 -0700

    build: Add DCR client to RestApiPoller

--- RestApiPoller/Program.cs
+++ RestApiPoller/Program.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics;
-using MsSentinel.ObservabilityDemo.ApiService.Client;
 using MsSentinel.ObservabilityDemo.RestApiPoller;
 
 var builder = Host.CreateApplicationBuilder(args);
@@ -12,13 +11,19 @@ builder.Services.AddHostedService<Worker>();
 builder.Services.AddSingleton(
     new ActivitySource("MsSentinel.ObservabilityDemo.RestApiPoller", "1.0.0"));
 
-builder.Services.AddHttpClient<ApiClient>(client =>
+builder.Services.AddHttpClient<MsSentinel.ObservabilityDemo.ApiService.Client.ApiClient>(client =>
     {
         // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
         // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
         client.BaseAddress = new("https+http://ApiService");
     });
 
+builder.Services.AddHttpClient<MsSentinel.ObservabilityDemo.DataCollectionRule.Client.ApiClient>(client =>
+    {
+        // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
+        // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
+        client.BaseAddress = new("https+http://DataCollectionRule");
+    });
 
 var host = builder.Build();
 await host.RunAsync();
