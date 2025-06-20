35bbc48c88df5fd27d4b04b646c5d02405ff1f24
James Coliz <jcoliz@outlook.com>
2025-06-20 13:40:24 -0700

    build: dotnet new worker -f net9.0 -o RestApiPoller -n MsSentinel.ObservabilityDemo.RestApiPoller

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -3,6 +3,11 @@ var builder = DistributedApplication.CreateBuilder(args);
 var apiService = builder.AddProject<Projects.MsSentinel_ObservabilityDemo_ApiService>("apiservice")
     .WithEnvironment("Logging__Console__FormatterName","systemd");
 
+builder.AddProject<Projects.MsSentinel_ObservabilityDemo_RestApiPoller>("RestApiPoller")
+    .WithReference(apiService)
+    .WaitFor(apiService)
+    .WithEnvironment("Logging__Console__FormatterName","systemd");
+
 #if false
 builder.AddProject<Projects.MsSentinel_ObservabilityDemo_Web>("webfrontend")
     .WithExternalHttpEndpoints()
