9a6e9eaa5f6f7bdda6b37a21b4431cd82a628b34
James Coliz <jcoliz@outlook.com>
2025-06-20 09:58:06 -0700

    build: Remove web frontend

--- MsSentinel.ObservabilityDemo.AppHost/Program.cs
+++ MsSentinel.ObservabilityDemo.AppHost/Program.cs
@@ -2,9 +2,11 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.MsSentinel_ObservabilityDemo_ApiService>("apiservice");
 
+#if false
 builder.AddProject<Projects.MsSentinel_ObservabilityDemo_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(apiService)
     .WaitFor(apiService);
+#endif
 
 builder.Build().Run();
