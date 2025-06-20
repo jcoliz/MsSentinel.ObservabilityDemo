4ae9944fc0d2ac7955132df85e954e12fe3d7918
James Coliz <jcoliz@outlook.com>
2025-06-20 10:30:36 -0700

    build: Add Swagger UI

--- ApiService/Program.cs
+++ ApiService/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Http.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add service defaults & Aspire client integrations.
@@ -9,10 +11,31 @@ builder.Services.AddProblemDetails();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(options =>
+{
+    options.Title = "MsSentinel.ObservabilityDemo.ApiService";
+    options.Description = "Synthetic API service for the MsSentinel Observability Demo";
+});
+
+// Tune the JSON formatting
+// https://www.meziantou.net/configuring-json-options-in-asp-net-core.htm
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.PropertyNameCaseInsensitive = false;
+    options.SerializerOptions.PropertyNamingPolicy = null;
+    options.SerializerOptions.WriteIndented = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseExceptionHandler();
+
+// Add swagger UI
+app.UseOpenApi();
+app.UseSwaggerUi();
 
 if (app.Environment.IsDevelopment())
 {
